63ce6365a9af4c1fb9add2c5bddd139c91a5b17c
Julia Augustinho <julia.augustinho@fatec.sp.gov.br>
2025-03-17 16:53:09 -0300

    feat: implementa base menu

---
+++ menu.py
@@ -0,0 +1,12 @@
+import calculadora
+
+def menu():
+    while True: # loop vai rodar indefinidamente. Para sair do loop, usamos um comando break
+        print("\nMenu Operações Matemáticas: ")
+        print("1. Somar")
+        print("2. Subtrair")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("0. Sair")
+
+        operacao = input("Escolha uma das opções das operações matemáticas, que consta no menu: ")
